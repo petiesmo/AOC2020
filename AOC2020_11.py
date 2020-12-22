d0ec81a26b2ee632af738005780cf28cb36058e3
petiesmo <66927015+petiesmo@users.noreply.github.com>
2020-12-22 01:21:28 -0700

    Create AOC2020_11.py

---
+++ AOC2020_11.py
@@ -0,0 +1,29 @@
+#Part 1: Find which outer bags could contain a 'shiny gold' bag
+
+def neighbors(x,y):
+    drow = [-1,0,1]
+    dcol = [-1,0,1]
+    
+    
+    
+    return new_state
+
+
+def main(start=''):
+    #global winners
+        
+    with open(r'C:\Users\pjsmole\Documents\GitHub\AOC2020\AOC2020_11.inp','r') as f:
+        inp = f.readlines()
+    
+    inp2 = [line.rstrip('\n') for line in inp]
+    seats = listoflistswithpaddedzeros
+    #Answer 1
+    
+    #Answer 2
+    
+    
+    print(f'Number of bags inside shiny gold is {bagcount}')
+
+if __name__ == '__main__':
+    main('posh brown')
+
